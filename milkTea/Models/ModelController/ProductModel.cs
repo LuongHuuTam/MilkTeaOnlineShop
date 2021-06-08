59f8d359d6b2ff67060dfa9ad0618115a4a16b57
Hung Do <beekunfar@gmai.com>
2021-06-08 19:50:38 +0700

    commit to save state

--- milkTea/Models/ModelController/ProductModel.cs
+++ milkTea/Models/ModelController/ProductModel.cs
@@ -117,7 +117,11 @@ namespace milkTea.Models.ModelController
         public string getNameByCatID(int id)
         {
             var res = _context.Categories.Where(x => x.CatId == id).SingleOrDefault();
-            return res.CatName;
+            if (res != null)
+            {
+                return res.CatName;
+            }
+            return null;
         }
 
         public IEnumerable<Products_Detail> searchProducts(string serch, int page, int pagesize)
